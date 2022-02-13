817c144baa47fdce5088934f062eef48fadb01aa
Pitu7944 <Pitu7944@gmail.com>
2022-02-14 00:44:56 +0100

    update and add configuration

--- src/extension.ts
+++ src/extension.ts
@@ -13,65 +13,84 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('cfxcli.restartCurrentScript', () => {
+	const disposable = vscode.commands.registerCommand('extension.restartCurrentScript', () => {
 		const vars = {
 			fileDirname: (vscode.window.activeTextEditor) ? path.dirname(vscode.window.activeTextEditor.document.fileName) : null,
 		};
 		const scriptName: any = vars['fileDirname']?.substring(vars['fileDirname']?.lastIndexOf('\\') + 1);
-		axios.post("http://127.0.0.1:30120/vscodebuttons/restart", {api_key: 'test_key', target_script: scriptName}).then(function (response: { data: any; }) {
-			//decode data to json
-			const data = response.data;
-			console.log(data);
-			if (data.success) {
-				vscode.window.showInformationMessage(data.message);
-			} else {
-				vscode.window.showInformationMessage(data.message);
-			}
-		}).catch(function (error: any) {
-			vscode.window.showInformationMessage(error);
-			console.error(error);
-		});
+		if (vscode.window.activeTextEditor) {
+			const currentDocument = vscode.window.activeTextEditor.document;
+			const configuration = vscode.workspace.getConfiguration('', currentDocument.uri);
+			const config_serverIP = configuration.get('cfxcli.ip', "");
+			const config_api_key = configuration.get('cfxcli.api_key', "");
+			axios.post(`${config_serverIP}/restart`, {api_key: config_api_key, target_script: scriptName}).then(function (response: { data: any; }) {
+				//decode data to json
+				const data = response.data;
+				console.log(data);
+				if (data.success) {
+					vscode.window.showInformationMessage(data.message);
+				} else {
+					vscode.window.showInformationMessage(data.message);
+				}
+			}).catch(function (error: any) {
+				vscode.window.showInformationMessage(error);
+				console.error(error);
+			});
+		}
 	});
 
-	const disposable2 = vscode.commands.registerCommand('cfxcli.startCurrentScript', () => {
+	const disposable2 = vscode.commands.registerCommand('extension.startCurrentScript', () => {
 		const vars = {
 			fileDirname: (vscode.window.activeTextEditor) ? path.dirname(vscode.window.activeTextEditor.document.fileName) : null,
 		};
 		const scriptName: any = vars['fileDirname']?.substring(vars['fileDirname']?.lastIndexOf('\\') + 1);
-		axios.post("http://127.0.0.1:30120/vscodebuttons/start", {api_key: 'test_key', target_script: scriptName}).then(function (response: { data: any; }) {
-			//decode data to json
-			const data = response.data;
-			console.log(data);
-			if (data.success) {
-				vscode.window.showInformationMessage(data.message);
-			} else {
-				vscode.window.showInformationMessage(data.message);
-			}
-		}).catch(function (error: any) {
-			vscode.window.showInformationMessage(error);
-			console.error(error);
-		});
+		if (vscode.window.activeTextEditor) {
+			const currentDocument = vscode.window.activeTextEditor.document;
+			const configuration = vscode.workspace.getConfiguration('', currentDocument.uri);
+			const config_serverIP = configuration.get('cfxcli.ip', "");
+			const config_api_key = configuration.get('cfxcli.api_key', "");
+			axios.post(`${config_serverIP}/start`, {api_key: config_api_key, target_script: scriptName}).then(function (response: { data: any; }) {
+				//decode data to json
+				const data = response.data;
+				console.log(data);
+				if (data.success) {
+					vscode.window.showInformationMessage(data.message);
+				} else {
+					vscode.window.showInformationMessage(data.message);
+				}
+			}).catch(function (error: any) {
+				vscode.window.showInformationMessage(error);
+				console.error(error);
+			});
+		}
 	});
 
-	const disposable3 = vscode.commands.registerCommand('cfxcli.stopCurrentScript', () => {
+	const disposable3 = vscode.commands.registerCommand('extension.stopCurrentScript', () => {
 		const vars = {
 			fileDirname: (vscode.window.activeTextEditor) ? path.dirname(vscode.window.activeTextEditor.document.fileName) : null,
 		};
 		const scriptName: any = vars['fileDirname']?.substring(vars['fileDirname']?.lastIndexOf('\\') + 1);
-		axios.post("http://127.0.0.1:30120/vscodebuttons/stop", {api_key: 'test_key', target_script: scriptName}).then(function (response: { data: any; }) {
-			//decode data to json
-			const data = response.data;
-			console.log(data);
-			if (data.success) {
-				vscode.window.showInformationMessage(data.message);
-			} else {
-				vscode.window.showInformationMessage(data.message);
-			}
-		}).catch(function (error: any) {
-			vscode.window.showInformationMessage(error);
-			console.error(error);
-		});
+		if (vscode.window.activeTextEditor) {
+			const currentDocument = vscode.window.activeTextEditor.document;
+			const configuration = vscode.workspace.getConfiguration('', currentDocument.uri);
+			const config_serverIP = configuration.get('cfxcli.ip', "");
+			const config_api_key = configuration.get('cfxcli.api_key', "");
+			axios.post(`${config_serverIP}/stop`, {api_key: config_api_key, target_script: scriptName}).then(function (response: { data: any; }) {
+				//decode data to json
+				const data = response.data;
+				console.log(data);
+				if (data.success) {
+					vscode.window.showInformationMessage(data.message);
+				} else {
+					vscode.window.showInformationMessage(data.message);
+				}
+			}).catch(function (error: any) {
+				vscode.window.showInformationMessage(error);
+				console.error(error);
+			});
+		}
 	});
-
+	context.subscriptions.push(disposable2);
+	context.subscriptions.push(disposable3);
 	context.subscriptions.push(disposable);
 }
