fd148238c1e643b953d134cd6bb5c38d3a487ac1
Pitu7944 <Pitu7944@gmail.com>
2022-02-14 11:13:34 +0100

    fix find_fxmanifest function

--- src/extension.ts
+++ src/extension.ts
@@ -5,26 +5,33 @@ import * as path from 'path';
 import * as fs from 'fs';
 import axios from 'axios';
 
-function find_fxmanifest(scriptPath: string, currentDepth = 0) {
-	fs.readdir(scriptPath, (err, files: string[]) => {
-		let foundManifest = false;
-		files.forEach((file: string) => {
-			if (file.includes("__resource.lua") || file.includes("fxmanifest.lua")) {
-				console.log("znaleziono fxmanifest.lua lub __resource.lua");
-				console.log(`znaleziono w ${scriptPath}`);
-				foundManifest = true;
-				return file;
-			}
-		});
-		if (!foundManifest && currentDepth < 10) {
-			const currentpath = path.join(scriptPath, '../');
-			console.log(currentpath);
-			find_fxmanifest(currentpath, currentDepth + 1);
-		} else if(currentDepth >= 10) {
-			console.log("Nie znaleziono fxmanifest.lua lub __resource.lua");
-			vscode.window.showInformationMessage("Nie znaleziono fxmanifest.lua lub __resource.lua");
+
+
+function find_fxmanifest(scriptPath: string, currentDepth = 0): string {
+	let foundManifest = false;
+	const dir = fs.readdirSync(scriptPath);
+	let result = "";
+	for (let i = 0; i < dir.length; i++) {
+		const file = dir[i];
+		if (file.includes("__resource.lua") || file.includes("fxmanifest.lua")) {
+			console.log("znaleziono fxmanifest.lua lub __resource.lua");
+			console.log(`znaleziono w ${scriptPath}`);
+			foundManifest = true;
+			return scriptPath;
 		}
-	});
+	}
+	if (!foundManifest && currentDepth < 10) {
+		const currentpath = path.join(scriptPath, '../');
+		console.log(currentpath);
+		result = find_fxmanifest(currentpath, currentDepth + 1);
+	} else if(currentDepth >= 10) {
+		console.log("Nie znaleziono fxmanifest.lua lub __resource.lua");
+		vscode.window.showInformationMessage("Nie znaleziono fxmanifest.lua lub __resource.lua");
+	} else if(foundManifest){
+		console.log("znaleziono fxmanifest.lua lub __resource.lua w katalogu "+ scriptPath);
+		result = scriptPath;
+	}
+	return result;
 }
 
 // this method is called when your extension is activated
@@ -40,7 +47,10 @@ export function activate(context: vscode.ExtensionContext) {
 			const configuration = vscode.workspace.getConfiguration('', currentDocument.uri);
 			const config_serverIP = configuration.get('cfxcli.ip', "");
 			const config_api_key = configuration.get('cfxcli.api_key', "");
-			const scriptName = find_fxmanifest(scriptPath);
+			const fxmanifestpath = find_fxmanifest(scriptPath);
+			const scriptName = fxmanifestpath.substring(fxmanifestpath.lastIndexOf('\\') + 1);
+			console.log("fxmanifestpath: " + fxmanifestpath);
+			console.log("scriptname: " + scriptName);
 			axios.post(`${config_serverIP}/restart`, {api_key: config_api_key, target_script: scriptName}).then(function (response: { data: any; }) {
 				//decode data to json
 				const data = response.data;
